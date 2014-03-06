372bb82a679ef4af913f1d5b0deb77b420a16f8d
wetoolaguer <wenceslaoolaguer@gmail.com>
2014-03-06 22:06:52 +0800

    Works on DiffRobot

--- diffRobot.js
+++ diffRobot.js
@@ -1,12 +1,21 @@
-function DiffRobot () {
-    function init () {
-        console.log("DiffRobot created.");
-    }
-    
-    function diff (image1, image2) {
-    }
+function DiffRobot (baseImgDir, newImgDir) {
+    this.baseImgDir = baseImgDir || 'baseImgs/';
+    this.newImgDir = newImgDir || 'newImgDir/';
 
+    var init = function () {
+        console.log("DiffRobot created.");
+    };
+    
     init();
 }
+
+DiffRobot.prototype.diffImage = function (image1, image2) {
+};
+
+DiffRobot.prototype.drawDiffImage = function () {
+}; 
+
+DiffRobot.prototype.saveImageDiff = function (fileName) {
+};
 
 module.exports = DiffRobot;
